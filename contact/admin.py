e769f0c9f70e7f345f7bbb43213cd08b7a8bf12d
Alessandro Pereira <alessandrop332@gmail.com>
2024-04-05 10:36:45 -0300

    Models desenvolvido

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from contact.models import Contact
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'nome', 'sobrenome', 'phone',
+    ordering = 'id',
+    #list_filter = 'create_data',
+    search_fields = 'id', 'nome', 'sobrenome',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = 'nome', 'sobrenome',
+    list_display_links = 'id', 'phone',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    sobrenome = models.CharField('Sobrenome', max_length=50,  blank=True)
+    phone = models.CharField('Phone', max_length=50)
+    email = models.EmailField('E-mail', max_length=250, blank=True)
+    create_data = models.DateTimeField(default=timezone.now)
+    descrição = models.TextField(blank=True)
+    mostrar = models.BooleanField(default=True)
+    imagem = models.ImageField(blank=True, upload_to='imagens/%y/%m/')
+
+
+    def __str__(self) -> str:
+        return f'{self.nome} {self.sobrenome}'
+
