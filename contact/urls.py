e09ea432a192e7e4542b3f363ed3b14e65320ee2
Alessandro Pereira <alessandrop332@gmail.com>
2024-04-16 12:45:38 -0300

    Modificando e definindo barra de pesquisa da lista de contatos, criando a paginação para navegação e também integrado com a pesquisa do usuario.

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contato_id>/', views.contato, name='contato'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,53 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
 from django.http import Http404
 
 from contact.models import Contact
+
 # Create your views here.
 def index(request):
     contacts = Contact.objects\
         .filter(mostrar=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+
+    context = {
+        'page_obj' : page_obj,
+        'site_title' : 'Contatos - '
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+def search (request):
+    search_value = request.GET.get('q','').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects\
+        .filter(mostrar=True)\
+        .filter(
+            Q(nome__icontains=search_value) |
+            Q(sobrenome__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     
     context = {
-        'contacts' : contacts,
+        'page_obj' : page_obj,
         'site_title' : 'Contatos - '
     }
     return render(
